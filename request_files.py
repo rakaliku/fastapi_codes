60c57e07d81437c5209b75c30bdf13701c1fadbb
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2025-01-19 22:09:04 +0530

    new code added

---
+++ request_files.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file size": len(file)}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
